6203a8c4f362eedb96cf62a8112e25f3ed5ff08f
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2023-10-16 12:04:17 +0200

    Update numbers.ts

--- src/modules/utils/numbers.ts
+++ src/modules/utils/numbers.ts
@@ -1,6 +1,4 @@
-// const getRandomUUID = (a: number) =>
-//     a ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, getRandomUUID);
-const numbers = {
+export const numbers = {
     /** The maximum is exclusive and the minimum is inclusive
      * @param min  (inclusive)
      * @param max  (exclusive)
@@ -12,35 +10,110 @@ const numbers = {
         return Math.floor(Math.random() * (max - min) + min);
     },
     /** Generate a Random UUID: 128-bit value; */
-    // getRandomUUID,
+    getRandomUUID,
     /** Timer */
-    timer: {
-        start(): Date {
-            return new Date();
-        },
-        stop(startTime: number, hours: number, minutes: number, seconds: number, hundredths: number) {
-            const diff = new Date().getDate() - startTime;
-            const text = this.text(diff, hours, minutes, seconds, hundredths);
-            return {
-                diff,
-                text,
-            };
-        },
-        text(diff: number, hours: number, minutes: number, seconds: number, hundredths: number): string {
-            const sec = 1000,
-                min = 60 * sec,
-                hrs = 60 * min;
-            let text = '';
-            text += hours ? `${Math.floor(diff / hrs)}:` : '';
-            text += `${Math.floor((diff % hrs) / min).toLocaleString('de-DE', {minimumIntegerDigits: 2})}`;
-            text += seconds ? `:${Math.floor((diff % min) / sec).toLocaleString('de-DE', {minimumIntegerDigits: 2})}` : '';
-            text +=
-                seconds && hundredths
-                    ? `.${Math.floor(diff % sec).toLocaleString('de-DE', {minimumIntegerDigits: 4, useGrouping: false})}`
-                    : '';
-            return text;
-        },
-    },
+    timer: () => new Timer(),
 };
+
+function getRandomUUID(): string {
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+    return uuid;
+}
+
+export enum TimerOption {
+    hours = 1,
+    minutes = 2, // allays in
+    seconds = 4,
+    milliseconds = 8,
+}
+type TimerResult = {
+    time: number;
+    label: string;
+};
+
+export class Timer {
+    private static DEFAULT_OPTIONS: number = TimerOption.minutes + TimerOption.seconds;
+    private static LOCALE = 'de-DE';
+    private static DIGITS = 2;
+    private static DIGITS_HUNDREDTHS = 4;
+    private static USE_GROUPING = false;
+    private startTime!: number;
+    private endTime!: number;
+    private flags: number;
+
+    constructor() {
+        this.start();
+        this.flags = Timer.DEFAULT_OPTIONS;
+    }
+    start(): void {
+        this.startTime = new Date().getTime();
+        this.endTime = this.startTime;
+    }
+    stop(): void {
+        this.endTime = new Date().getTime();
+    }
+    setOptions(...args: TimerOption[]): void {
+        if (args.length === 0) {
+            this.flags = Timer.DEFAULT_OPTIONS;
+        } else {
+            this.flags = args.reduce((a, b) => a + b, 0);
+        }
+    }
+    private isTrue(bitmask: TimerOption): boolean {
+        return (this.flags & bitmask) > 0;
+    }
+    result(...args: TimerOption[]): TimerResult {
+        // save options
+        const flags_save = this.flags;
+
+        // time in milliseconds
+        const time = this.endTime - this.startTime;
+
+        // result with custom options
+        if (args.length > 0) {
+            this.setOptions(...args);
+        }
+
+        // create label
+        const SEC: number = 1000;
+        const MIN: number = SEC * 60;
+        const HRS: number = MIN * 60;
+        let label: string = '';
+
+        // hours (Optional)
+        if (this.isTrue(TimerOption.hours)) {
+            label = label.concat(Number(Math.floor(time / HRS)).toString(), ':');
+        }
+
+        // minutes
+        label = label.concat(Math.floor((time % HRS) / MIN).toLocaleString(Timer.LOCALE, {minimumIntegerDigits: Timer.DIGITS}));
+
+        // seconds (Optional)
+        if (this.isTrue(TimerOption.seconds) || this.isTrue(TimerOption.milliseconds)) {
+            label = label.concat(':', Math.floor((time % MIN) / SEC).toLocaleString(Timer.LOCALE, {minimumIntegerDigits: Timer.DIGITS}));
+        }
+
+        // milliseconds (Optional)
+        if (this.isTrue(TimerOption.milliseconds)) {
+            label = label.concat(
+                '.',
+                Math.floor(time % SEC).toLocaleString(Timer.LOCALE, {
+                    minimumIntegerDigits: Timer.DIGITS_HUNDREDTHS,
+                    useGrouping: Timer.USE_GROUPING,
+                }),
+            );
+        }
+        this.flags = flags_save;
+
+        return {
+            time,
+            label,
+        };
+    }
+}
 
 export default numbers;
